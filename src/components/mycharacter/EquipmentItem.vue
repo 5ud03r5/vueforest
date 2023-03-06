b5b864c9b2e0385c785dbfcf03a906916631ed1d
5ud03r5 <kmarek973@gmail.com>
2023-03-06 09:49:40 +0100

    modified image class and src

--- src/components/mycharacter/EquipmentItem.vue
+++ src/components/mycharacter/EquipmentItem.vue
@@ -1,18 +1,20 @@
 <template>
     <div :class="itemClass">
-        <img :src="`src/assets/${itemName}.svg`" :alt="item" class="h-10" />
+        <img :src="getImageUrl(itemName)" :alt="item" class="h-10" />
     </div>
 </template>
 <script>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 export default {
     props: ['item'],
     setup(props) {
 
         const itemName = ref(props.item.name)
         const itemClass = ref('bg-gray-200 m-1 hover:cursor-pointer hover:bg-white relative')
-
-        return { itemClass, itemName }
+        const getImageUrl = (name) => {
+            return new URL(`../../assets/${name}.svg`, import.meta.url).href
+        }
+        return { itemClass, itemName, getImageUrl }
     }
 }
 
